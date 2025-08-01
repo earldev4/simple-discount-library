dd3e9ff8d3eddab69083ae9fbce24c4f38b213ee
Deva Ahmad <devaahmad43@gmail.com>
2025-08-01 21:20:13 +0700

    Simple Discount Library

---
+++ src/discount/Discount.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace EarlDev\PhpComposer1;
+class Discount {
+
+    public function hitungHargaAkhir($hargaAwal, $diskon): float|int {
+        return $hargaAwal - ($hargaAwal * $diskon / 100);
+    }
+
+    public function hitungHargaAwal($hargaAkhir, $diskon): float|int {
+        return $hargaAkhir / (1 - ($diskon / 100)); 
+    }
+}
